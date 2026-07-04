b512924580d76d41eaa41fe2bdc5ea7c5e8fdc99
Md Shadab Azam ANsari <shadabazama@gmail.com>
2026-07-05 00:40:20 +0530

    e

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -6,6 +6,7 @@ const sesClient = new SESClient({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    ...(process.env.AWS_SESSION_TOKEN && { sessionToken: process.env.AWS_SESSION_TOKEN }),
   },
 });
 
